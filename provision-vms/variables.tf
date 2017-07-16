c7a0d147c1f14144ee97c133a0b4a2084b557c6f
mikegcoleman <mikegcoleman@gmail.com>
2017-07-16 13:01:40 -0400

    removed terraform variable defaults

--- provision-vms/variables.tf
+++ provision-vms/variables.tf
@@ -3,55 +3,64 @@
 variable "resource_group" {
   description = "Name of the resource group to use for VMs"
   type = "string"
+# add the name of the resource group you want created (cannot exist)
   default = "mike-pa"
 }
 
 variable "storage_account" {
   description = "Name of the storage account to use for VMs"
   type = "string"
-  default = "mikepa"
+# add the name of the storage account you want created (cannot exist)
+  default = ""
 }
 
 variable "dns_prefix" {
   description = "DNS prefix for VM public addreses"
   type = "string"
-  default = "pa"
+# This string is used as the first part of dns name eg: pdx-lin-01 or pdx-win-01
+  default = ""
 }
 
 variable "region" {
   description = "Azure region to use"
   type = "string"
-  default = "westus2"
+# Supply a default region (e.g. westus2) 
+  default = ""
 }
 
 variable "win_image_vhd_uri" {
   description = "URI of the windows VHD to use as the VM image"
   type = "string"
-  default = "https://mikergdisks645.blob.core.windows.net/vhds/win2016-17-05-osDisk.2eb0a541-44f5-44a4-8416-0280d4976722.vhd?st=2017-05-25T00%3A44%3A00Z&se=2018-05-26T00%3A44%3A00Z&sp=rl&sv=2016-05-31&sr=b&sig=wSbPKamsKmlTuqPVZPEt44Y8MIGR5taf%2BykNgwmt%2B70%3D"
+# you need to supply a link to a valid Windows Azure image
+  default = ""
 }
 
 variable "lin_image_vhd_uri" {
   description = "URI of the linux VHD to use as the VM image"
   type = "string"
-  default = "https://mikergdisks645.blob.core.windows.net/vhds/linux-base20170523165353.vhd?st=2017-05-24T06%3A06%3A00Z&se=2018-05-25T06%3A06%3A00Z&sp=rl&sv=2016-05-31&sr=b&sig=9hLOky1FufB0u60pojAqUgeej7iZRrHxloZ%2Fi7FSs38%3D"
+# you need to supply a link to a valid Linux Azure image
+  default = ""
 }
 
 variable "win_vm_count" {
   description = "Number of VMs to create"
   type = "string"
-  default = "100"
+# specify the number of Windows VMs to create
+  default = ""
 }
 
 variable "lin_vm_count" {
   description = "Number of VMs to create"
   type = "string"
-  default = "100"
+# specify the number of Linux VMs to create
+  default = ""
 }
 
 variable "vm_size" {
   description = "VM size to create"
   type = "string"
-  default = "Standard_DS1_v2"
+# specify the VM size to create (e.g. Standard_DS1_v2)
+  default = ""
 }
 
 variable "azure_dns_suffix" {
