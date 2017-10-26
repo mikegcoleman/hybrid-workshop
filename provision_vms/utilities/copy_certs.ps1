6f7e860382d2ab7104eb780906e1762b7d7893d0
Matt Bentley <matt.bentley@docker.com>
2017-10-26 16:21:18 -0400

    Added back settings to ignore invalid certs
    Signed-off-by: Matt Bentley <matt.bentley@docker.com>

--- provision_vms/utilities/copy_certs.ps1
+++ provision_vms/utilities/copy_certs.ps1
@@ -1,5 +1,22 @@
 param([string]$DTR_URL)
 
+add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class IDontCarePolicy : ICertificatePolicy {
+        public IDontCarePolicy() {}
+        public bool CheckValidationResult(
+            ServicePoint sPoint, X509Certificate cert,
+            WebRequest wRequest, int certProb) {
+            return true;
+        }
+    }
+"@
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+[System.Net.ServicePointManager]::CertificatePolicy = new-object IDontCarePolicy
+
 Write-Host "`nDownloading DTR self-signed CA certificate from https://$DTR_URL/ca..."
 Invoke-WebRequest -uri "https://$DTR_URL/ca" -o c:\ca.crt
 Write-Host "done.`n"
